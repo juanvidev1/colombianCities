e2341e191b3c4bfb1a4623efd1df8cd8d59fd6db
juanvidev1 <juanvidev1@gmail.com>
2023-10-02 16:18:14 -0500

    feat: Add somo information about the WS

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json(
+        {
+            message: "Bienvenido a Mi Servicio de Ciudades",
+            version: "1.0.0",
+            description: "Un servicio web para consultar información sobre ciudades y departamentos.",
+            documentation: "https://example.com/documentation",
+            github_repository: "https://github.com/tu-usuario/tu-repositorio"
+        }
+    );
+});
+
 app.listen(PORT, () => {
     console.log('Server running on port ' + PORT);
 });
